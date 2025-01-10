17b894359f115187f935a12e5bb28fad6a75f08c
Maria Kazakova <mariakazakova92@gmail.com>
2025-01-10 23:38:35 +0300

    added click on the close button

--- scripts/openWindow.js
+++ scripts/openWindow.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.querySelector(".burgerMenu");
+    const windowContainer = document.querySelector(".menuWindow__container");
     const menuWindow = document.querySelector(".menuWindow");
+    const closeWindow = document.querySelector(".closeWindow");
 
     burgerMenu.addEventListener("click", function () {
-        // Переключение состояния активного меню
         burgerMenu.classList.toggle("active");
         menuWindow.classList.toggle("open");
+        windowContainer.classList.toggle("open");
     });
+
+    closeWindow.addEventListener("click", function () {
+        burgerMenu.classList.remove("active");
+        menuWindow.classList.remove("open");
+        windowContainer.classList.remove("open");
+    })
 });
